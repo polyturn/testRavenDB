9183ab8fa6b7c33e4c728092844cab560b5373ee
polyturn <kleun56@yahoo.com>
2014-05-02 22:50:20 -0400

    init commit

---
+++ testRavenDB/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Raven.Client.Document;
+using Raven.Client.Indexes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace testRavenDB
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static DocumentStore MyStore;
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MyStore = new DocumentStore { ConnectionStringName = "RavenDB" };
+            MyStore.Initialize();
+
+            IndexCreation.CreateIndexes(Assembly.GetCallingAssembly(), MyStore);
+        }
+    }
+}
